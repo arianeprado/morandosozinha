3f41603b7efe4617d7e2fabade29a98e1d21616b
arianeprado <ariiane.prado@gmail.com>
2014-03-27 02:20:37 -0300

    todas as páginas

---
+++ README.md
@@ -0,0 +1,4 @@
+fatecscript
+===========
+morandosozinha
+==============
---
+++ backend/src/web/my_form.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
+
+def index(_write_tmpl, name):
+    _write_tmpl('templates/form.html', {'name': name})
